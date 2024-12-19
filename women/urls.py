fc418bef57f95e1d78dcb9b97e281bbd0bc88877
Zimenkov <kupolzim@yandex.ru>
2024-12-19 10:53:28 +0300

    V 1.1.2

--- women/urls.py
+++ women/urls.py
@@ -7,9 +7,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
-    path('women/', views.index),
-    path('cats/<int:cat_id>/', views.categories),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug),
-    path("archive/<year4:year>/", views.archive),
-    path('', views.major),
+    path('', views.index, name='home'),
+    path('cats/<int:cat_id>/', views.categories, name='cats_id'),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    path("archive/<year4:year>/", views.archive, name='archive'),
+    path("posts_list/<int:year>/", views.posts_list),
 ]
--- women/views.py
+++ women/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('страница приложения women.')
+    return HttpResponse('<h1>Супер главная страница<h1/>')
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статьи по категориям<h1/><p>cat_id {cat_id}</p>')
@@ -16,8 +18,10 @@ def categories_by_slug(request, cat_slug):
 
 def archive(request, year):
     if year > 2023:
-        raise Http404()
-    return HttpResponse(f'<h1>Архив по годам<h1/><p>year: {year}</p>')
+        url = reverse('cats', args=('music', ))
+        return redirect(url)
+
+    return HttpResponse(f"year: {year}")
 
 def posts_list(request, year):
     if  1990 <= year <= 2023:
