e40f30d21ca64b5b46c136a8b0aa3e8635c53e8f
Misha One <kupolzim@yandex.ru>
2025-01-16 21:21:13 +0300

    Ver1.1.5(21:20 16.01.2025)

--- Jin/test.py
+++ Jin/test.py
@@ -1,12 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
 
-class ShopItem(models.Model):
+class Auto(models.Model):
     name = models.CharField(max_length=100)
-    weight = models.IntegerField(blank=True, default=0)
-    price = models.IntegerField(default=0)
+    model = models.CharField()
+    price = models.IntegerField(default=0, blank=True)
     is_exists = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+
+Auto.objects.create(name="BWM X6", model="BMW", price=6000111)
+Auto.objects.create(name="Toyota Corolla", model="Toyota")
+Auto.objects.create(name="Haval 7", model="Haval", price=3500222)
+
+autos = Auto.objects.all()[0]
+
+
