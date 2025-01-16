9e2b1f6f362c5e558557ab3b8a2caacbb724318c
Misha One <kupolzim@yandex.ru>
2025-01-16 14:15:09 +0300

    Ver1.1.5(22:00 16.01.2025)

--- Jin/test.py
+++ Jin/test.py
@@ -1,3 +1,12 @@
-c = {'a':'test', 'b':'b_test'}
+from django.db import models
 
-print('|'.join([f'{k}={v}' for k,v in c.items()]))
+# Create your models here.
+
+class ShopItem(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.IntegerField(blank=True, default=0)
+    price = models.IntegerField(default=0)
+    is_exists = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
