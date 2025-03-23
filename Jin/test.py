c9b14bc94de1e9b61fbe537026e6515a5a540c0e
Zimenkov <kupolzim@yandex.ru>
2025-03-23 09:34:23 +0300

    v 1.2.7(23.03.2025)

--- Jin/test.py
+++ Jin/test.py
@@ -37,3 +37,18 @@ class CommentForm(forms.Form):
     email = forms.EmailField()
     agree = forms.BooleanField()
     content = forms.CharField(widget=forms.Textarea)
+
+
+
+# fio: текстовое поле; максимальная длина 200 символов, обязательное, название "Владелец";
+# email: поле ввода адреса электронной почты; обязательное, название "E-mail";
+# vin: текстовое поле; максимальная длина 20 символов, обязательное, название "VIN";
+# model: текстовое поле; максимальная длина 100 символов, обязательное, название "Модель";
+# stag: числовое поле; необязательное, название "Стаж".
+
+class OsagoForm(forms.Form):
+    fio = forms.CharField(max_length=200, label='Владелец')
+    email = forms.EmailField(label='E-mail')
+    vin = forms.CharField(max_length=20, label='VIN')
+    model = forms.CharField(max_length=100, label='Модель')
+    stag = forms.IntegerField(required=False, label='Стаж')
