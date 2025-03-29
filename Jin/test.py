83964deca970220c14cac160de0c4002e13c75d1
Zimenkov <kupolzim@yandex.ru>
2025-03-29 15:09:01 +0300

    v 1.3.1(29.03.2025)

--- Jin/test.py
+++ Jin/test.py
@@ -137,3 +137,24 @@ class RegisterForm(forms.Form):
             raise ValidationError("Некорректно введенный пароль.")
         if len(p1) < 6:
             raise ValidationError("Слишком короткий пароль.")
+
+# Объявите класс формы PersonForm, связанной с моделью Person и следующими свойствами, прописанными во вложенном классе Meta:
+#
+# отображаемые поля (с сохранением порядка): full_name, salary, job;
+# поля full_name и job должны иметь CSS-стили attrs={'class': 'form-input'};
+# названия полей в HTML-форме: full_name -> "Полное имя"; salary -> "Зарплата"; job -> "Профессия".
+
+
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = ['full_name', 'salary', 'job']
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'job': forms.TextInput(attrs={'class': 'form-input'})
+        }
+        labels = {
+            'full_name': "Полное имя",
+            'salary': "Зарплата",
+            'job': "Профессия",
+        }
